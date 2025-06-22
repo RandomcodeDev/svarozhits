891a57d52bd46dbf3146439cd4caf1590d39fe2c
MobSlicer152 <brambleclaw1414@gmail.com>
2025-06-22 15:22:41 -0400

    Remove build folder, no idea how that got in

--- target/debug/deps/sve-3ef2f60350ab72c0.d
+++
@@ -1,7 +0,0 @@
-/home/mobslicer152/projects/svarozhits/target/debug/deps/sve-3ef2f60350ab72c0.d: common/sve/src/lib.rs
-
-/home/mobslicer152/projects/svarozhits/target/debug/deps/libsve-3ef2f60350ab72c0.rlib: common/sve/src/lib.rs
-
-/home/mobslicer152/projects/svarozhits/target/debug/deps/libsve-3ef2f60350ab72c0.rmeta: common/sve/src/lib.rs
-
-common/sve/src/lib.rs:
--- target/debug/deps/svetool-bb97ba34bcb5fbc2.d
+++
@@ -1,5 +0,0 @@
-/home/mobslicer152/projects/svarozhits/target/debug/deps/svetool-bb97ba34bcb5fbc2.d: tools/svetool/src/main.rs
-
-/home/mobslicer152/projects/svarozhits/target/debug/deps/svetool-bb97ba34bcb5fbc2: tools/svetool/src/main.rs
-
-tools/svetool/src/main.rs:
